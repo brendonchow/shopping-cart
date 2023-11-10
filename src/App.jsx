290b977580a249106d5b46e42dabe4c9b752b95e
Brendon Chow <brendonchow123@gmail.com>
2023-11-10 16:48:33 +1100

    Implement loading and error screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./components/Home.jsx";
 import Shop from "./components/Shop.jsx";
 import Cart from "./components/Cart.jsx";
@@ -7,10 +8,42 @@ import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
   const params = useParams();
-  console.log(params);
   const [cartItems, setCartItems] = useState([]);
+  const [shopItems, setShopItems] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let ignore = false;
+
+    fetch("https://fakestoreapi.com/products?limit=5", {
+      mode: "cors",
+    })
+      .then((res) => {
+        if (!res.ok || ignore) {
+          throw new Error(res);
+        }
+        return res.json();
+      })
+      .then((json) => setShopItems(json))
+      .catch((res) => {
+        if (!ignore) {
+          setError(res);
+        }
+      })
+      .finally(() => {
+        if (!ignore) {
+          setLoading(false);
+        }
+      });
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
   return params.page === "shop" ? (
-    <Shop>
+    <Shop error={error} loading={loading} items={shopItems}>
       <Navbar current="shop" numberOfItems={cartItems.length} />
     </Shop>
   ) : params.page === "cart" ? (
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,11 +1,26 @@
 import PropTypes from "prop-types";
 
-const Shop = ({ children }) => {
-  return <>{children}</>;
+const Shop = ({ children, error, loading, items }) => {
+  console.log(error, loading);
+  return (
+    <>
+      {children}
+      {error ? (
+        <div className="error">ERROR: {error.message}</div>
+      ) : loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        ""
+      )}
+    </>
+  );
 };
 
 Shop.propTypes = {
   children: PropTypes.object.isRequired,
+  error: PropTypes.instanceOf(TypeError),
+  loading: PropTypes.bool.isRequired,
+  items: PropTypes.object.isRequired,
 };
 
 export default Shop;
